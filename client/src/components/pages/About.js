92635b0b17da3060b7acadcfa83945e6fb312027
sale1234 <jevremovicsasa@gmail.com>
2021-08-10 12:06:49 +0200

    navbar added

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this page</h1>
+      <p className='my-1'>ContactKeeper ITEH APLIKACIJA</p>
+      <p className='bg-dark p'>
+        <strong>Verzija: 1.0.0</strong>
+      </p>
+    </div>
+  )
+}
+
+export default About
